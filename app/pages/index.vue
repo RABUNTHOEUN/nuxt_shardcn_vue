47df272c57928ba6951fb88b31258a3b17649a0c
RABUNTHOEUN <rabunthoeun7777@gmail.com>
2025-10-29 16:53:38 +0000

    update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,32 +1,15 @@
-<template>
-  <div>
-    <h1>Color mode: {{ $colorMode.value }}</h1>
-    <select v-model="$colorMode.preference">
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-      <option value="sepia">Sepia</option>
-    </select>
-  </div>
-</template>
-
-<script setup>
-const colorMode = useColorMode()
-
-console.log(colorMode.preference)
+<script lang="ts">
+export const description = "A simple login form."
 </script>
 
-<style>
-body {
-  background-color: #fff;
-  color: rgba(0,0,0,0.8);
-}
-.dark-mode body {
-  background-color: #091a28;
-  color: #ebf4f1;
-}
-.sepia-mode body {
-  background-color: #f1e7d0;
-  color: #433422;
-}
-</style>
+<script setup lang="ts">
+import LoginForm from '@/components/LoginForm.vue'
+</script>
+
+<template>
+  <div class="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div class="w-full max-w-sm">
+      <LoginForm />
+    </div>
+  </div>
+</template>
